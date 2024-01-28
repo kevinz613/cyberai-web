0fa6a6cb02d33997f625699d0b516decf911860c
fzhizhuang <fzhizhuang@163.com>
2024-01-28 21:14:28 +0800

    feat: 404页面

--- src/view/error/404.vue
+++ src/view/error/404.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div>
-    <img class="img" src="@/assets/error/404.svg" alt="404">
+    <img class="img" src="@/assets/error/404.svg" alt="404" />
   </div>
   <div class="content">
     <span>对不起，你访问的页面不存在</span>
@@ -13,16 +13,16 @@
 </template>
 
 <style scoped>
-.img{
+.img {
   width: 100%;
   height: 600px;
 }
-.content{
+.content {
   text-align: center;
   color: gray;
   font-size: 16px;
 }
-.btn{
+.btn {
   padding-top: 20px;
   text-align: center;
   font-size: 16px;
